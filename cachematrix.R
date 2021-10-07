71e0287b33baef4d708d0cfa801bfae81c600d20
Troll-desu <91359903+Troll-desu@users.noreply.github.com>
2021-10-07 15:01:13 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set the input x as a matrix
+## First part of code is for caching and assigning the cache name
 
-## Write a short comment describing this function
+##The assignment is for caching and inversing
+##First part is for caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##Special matrix created that caches its inverse is made with this function
+  invrs <- NULL 
+  ##Initializes invz as Null
+  ##Initializes hold value of matrix Inverse
+  set <- function(y){ ##set is funcion y
+    x <<- y  ##x is to y
+    invrs <<- NULL    ##set invrs to null with new data
+  }
+  get <- function()x{}    ##define the matrix func. as get
+  setInverse <- function(inverse) invrs <<- inverse ##inverse set as invrs
+  getInverse <- function() invrs{ ## gets the inverse value
+  list(set = set, get = get,setInverse = setInverse, 
+       getInverse = getInverse)   ##unequivocal
 }
-
-
-## Write a short comment describing this function
-
+## Second part inversing
+## message view inverse matrix value
+## Returning matrix from an inverse of x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Matrix return that is an inverse of x
+  invrs <- x$getInverse()
+  if(!is.null(invrs)){
+    message("Fetching cached data please wait")
+    return(invrs)
+  }
+  D <- x$get()
+  invrs <- solve(D,...)
+  x$setInverse(invrs)
+  invrs
 }
+
+##Hatdog ni Aljur
