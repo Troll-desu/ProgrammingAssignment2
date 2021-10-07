8019734e2ee99464dbd4e89e72f2e65588724397
Troll-desu <91359903+Troll-desu@users.noreply.github.com>
2021-10-07 14:49:01 +0800

    Update Lexical scoping assignment wk3.R

--- Lexical scoping assignment wk3.R
+++ Lexical scoping assignment wk3.R
@@ -1,27 +1,35 @@
-## set the input x as a matrix
+## Set the input x as a matrix
+## First part of code is for caching and assigning the cache name
+
+##The assignment is for caching and inversing
+##First part is for caching
+
 makeCacheMatrix <- function(x = matrix()) {
-  j <- NULL
+  ##Special matrix created that caches its inverse is made with this function
+  invrs <- NULL 
+  ##Initializes invz as Null
+  ##Initializes hold value of matrix Inverse
   set <- function(y){
     x <<- y
-    j <<- NULL
+    invrs <<- NULL
   }
-  get <- function()x
-  setInverse <- function(inverse) j <<- inverse
-  getInverse <- function() j 
+  get <- function()x{     ##define the matrix func. as get
+  setInverse <- function(inverse) invrs <<- inverse ##inverse set as invrs
+  getInverse <- function() invrs{ ## gets the inverse value
   list(set = set, get = get, 
        setInverse = setInverse, 
        getInverse = getInverse)
 }
 ## Returning matrix from an inverse of x
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Matrix return that is an inverse of x
   j <- x$getInverse()
-  if(!is.null(j)){
-    message("getting cached data")
-    return(j)
+  if(!is.null(invrs)){
+    message("Fetching cached data please wait")
+    return(invrs)
   }
-  mat <- x$get()
-  j <- solve(mat,...)
-  x$setInverse(j)
-  j
+  D <- x$get()
+  invrs <- solve(D,...)
+  x$setInverse(invrs)
+  invrs
 }
